954ee5fa7ca8814674da04a4738cf7fdb67c73ee
nyigoro <nyigoro@gmail.com>
2025-07-02 17:50:08 +0300

    Delete electron-app/ipc.js

--- electron-app/ipc.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('gare', {
-  runEngine: (payload) => ipcRenderer.send('run-engine', payload),
-  onLog: (callback) => ipcRenderer.on('log-update', (_, data) => callback(data)),
-});
