cc2e732d10525a2fc68073cddd1111e6eebc1010
nyigoro <nyigoro@gmail.com>
2025-07-05 01:05:14 +0300

    Update App.jsx

--- electron-app/src/components/App.jsx
+++ electron-app/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Terminal } from 'xterm';
 import 'xterm/css/xterm.css';
-import '@tailwindcss';
+import 'tailwindcss';
 
 export default function App() {
   const [activeTab, setActiveTab] = useState('containers');
