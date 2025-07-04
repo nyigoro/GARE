27b36c61af79a47fb7a9618f0975aad2cfc28ee7
nyigoro <nyigoro@gmail.com>
2025-07-05 00:40:55 +0300

    Update main.jsx

--- electron-app/src/main.jsx
+++ electron-app/src/main.jsx
@@ -1,10 +1,6 @@
-// electron-app/src/main.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
