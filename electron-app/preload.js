67ebc0a39f45840ba29cef23111ce5b35fc3705b
nyigoro <nyigoro@gmail.com>
2025-07-04 19:32:04 +0300

    Update preload.js

--- electron-app/preload.js
+++ electron-app/preload.js
@@ -1,7 +1,6 @@
-// electron-app/preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  runCommand: (data) => ipcRenderer.send('run-command', data),
-  onLog: (callback) => ipcRenderer.on('log', (_, msg) => callback(msg))
+  runCommand: (data) => ipcRenderer.invoke('run-command', data),
+  onLog: (callback) => ipcRenderer.on('log', (event, message) => callback(message)),
 });
