5c77c19ff8a3f3123acd5481e888127f2b2943f1
nyigoro <nyigoro@gmail.com>
2025-07-05 16:33:28 +0300

    Update main.jsx

--- electron-app/src/main.jsx
+++ electron-app/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App'; 
+import App from './components/App'; 
 
 // Get the root element from the HTML
 const container = document.getElementById('root');
